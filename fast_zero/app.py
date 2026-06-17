60ee2a8208e2dabe2ca52b7a90c60d1904a096cd
tenmenezes <tenn.menezes@gmail.com>
2026-06-16 21:16:27 -0300

    feat(schema and tests): Implementação de testes e exercício da aula 02 de fastAPI.

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,29 @@
+from http import HTTPStatus
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message:Olá Mundo!'}
+    return {'message': 'Olá Mundo!'}
+
+
+@app.get('/exercicio-html', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def exercicio_aula_02():
+    html_content = """
+        <html>
+            <head>
+                <title>Olá Mundo em HTML</title>
+            </head>
+            <body>
+                <h1>Olá Mundo</h1>
+                <p>Em HTML pelo próprio fastAPI.</p> 
+            </body>
+        </html>
+    """
+
+    return html_content
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,4 @@
+from curses import OK
 from http import HTTPStatus
 
 from fastapi.testclient import TestClient
@@ -6,9 +7,30 @@ from fast_zero.app import app
 
 
 def test_root_deve_retornar_ok_e_ola_mundo():
+
+    """
+    Teste de 3 Etapas (AAA)
+
+    - A: Arrange - Arranjo
+    - A: Act     - Executa a coisa (o SUT)
+    - A: Assert  - Garante que A é A
+
+    """
+    
+    # Arrange
     client = TestClient(app)
 
+    # Act
     response = client.get('/')
 
+    # Assert
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+def teste_deve_retornar_ok_e_olamundo_em_html():
+    client = TestClient(app)
+
+    response = client.get('/ola')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo </h1>' in response.text
